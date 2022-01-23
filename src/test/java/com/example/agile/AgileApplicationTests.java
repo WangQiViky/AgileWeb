b2c85997223c4297808dec5bbf0098bb75ff4560
wangqi <1209017023@qq.com>
2022-01-23 21:32:23 +0800

    初始化

---
+++ src/test/java/com/example/agile/AgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
