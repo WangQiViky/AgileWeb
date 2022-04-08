b2b7bb76b0db6475b58b97f63bd664ee45ca1c85
wangqi <1209017023@qq.com>
2022-04-08 18:26:17 +0800

    测试是否可以连到github

--- src/main/java/com/example/cinema/CinemaApplication.java
+++ src/main/java/com/example/cinema/CinemaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class CinemaApplication {
 
+    //测试一下啊，是否可以上传到github
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
     }
