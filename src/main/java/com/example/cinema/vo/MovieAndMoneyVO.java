7940c5b67e73483d0e90bd2889e077961f6a0581
wangqi <1209017023@qq.com>
2022-04-09 16:01:42 +0800

    增加movieAndMoneyVO，里面是对应的票房

---
+++ src/main/java/com/example/cinema/vo/MovieAndMoneyVO.java
@@ -0,0 +1,32 @@
+package com.example.cinema.vo;
+
+public class MovieAndMoneyVO {
+    private int movieId;
+    private String name;
+    private String money;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+}
